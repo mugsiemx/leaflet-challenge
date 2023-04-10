51a6e8f0eed08d813cb9b48b59da8ba35a34981e
mugsiemx <mugsiemx@gmail.com>
2023-04-09 22:46:52 -0400

    struggling with the circle markers

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,9 +1,28 @@
 // New Visualization for USGS Earthquake Data
 // Earthquakes for the last 7 days as of 4/8/2023 10:23 AM
-// relied heavily on code from Activity: 10-Stu_GeoJson in the 15-Mapping, Day 1, Activities Folder
+// Code from Activity: 10-Stu_GeoJson in the 15-Mapping, Day 1, Activities Folder
 
 // declare variables
 url = "data/all_week.geojson"
+
+//define functions changing defaults
+  // change color function for third coordinate (depth)
+function selectColor(depth) {
+  // set up color change by clearing previous color
+  var color = "";
+  // then passing the depth into the circle color function
+  if (depth > 90) {color = "Red"}
+    else if (depth > 70) {color = "Orange"}
+    else if (depth > 50) {color = "Salmon"}
+    else if (depth > 30) {color = "PaleGoldenRod"}
+    else if (depth > 10) {color = "GreenYellow"}
+    else if (depth > -10) {color = "LawnGreen"}
+    else {color = "Gold"}
+};
+  // change the circle sizes based on size of earthquake
+function sizeChange(mag) {
+  
+}
 
 // Perform a GET request to the query URL/
 d3.json(url).then(function (data) {
@@ -13,80 +32,29 @@ d3.json(url).then(function (data) {
 });
 
 function createFeatures(earthquakeData) {
-  // console.log(earthquakeData);
+  console.log(earthquakeData);
   // Define a function that we want to run once for each feature in the features array.
   // Give each feature a popup that describes the place and time of the earthquake.
-  
-  
-  
+    
   function doOnEachFeature(feature, layer) {
-    function circleMarkers(feature, latlng) {
-        // get the third element in the coordinates array for radius calculation  
-        var geometry = earthquakes[i].geometry;
-        var geoLat = geometry.coordinates[1];
-        var geoLng = geometry.coordinates[0];
-        var geoDepth = geometry.coordinates[2];
-        var depth = int(geoDepth) * 10;
-
-        var geojsonMarkerOptions = {
-            radius: depth,
-            color: selectColor(depth),
-            weight: 1,
-            opacity: 1,
-            fillOpacity: 0.8
-        };
+      
     // console.log(feature, layer),
-        layer.bindPopup(`<h1>${feature.properties.place}</h1><hr><p>${new Date(feature.properties.time)}</p>`);
-    // layer.bindPopup(`<h1>${feature.properties.place}</h1><hr><p>${new Date(feature.properties.time)}</p>`);
-  }};
+    layer.bindPopup(`<h1>${feature.properties.place}</h1><hr><p>${new Date(feature.properties.time)}</p>`);
+  }
 
   // Create a GeoJSON layer that contains the features array on the earthquakeData object.
   // Run the onEachFeature function once for each piece of data in the array.
   //parsing the data and has default features we can use for further processing.
   var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: doOnEachFeature,
-    pointToLayer: circleMarkers
+    onEachFeature: doOnEachFeature
   });
-
-  // console.log(earthquakes);
   // Send our earthquakes layer to the createMap function/
-  createMap(earthquakes);
+    createMap(earthquakes);
 }
 
-
-
-
 function createMap(earthquakes) {
-  var earthquakeMarkers = [];
+  // var earthquakeMarkers = [];
 
-
-
-
-  // Loop through the records, and create markers.
-  for (var i = 0; i < earthquakes.length; i++) {
-
-    // set up color change by clearing previous color
-    var color = "";
-    // then passing the third coordinate into the markerSize function
-    if (depth > 90) {color = "Red"}
-      else if (depth > 70) {color = "Red"}
-      else if (depth > 50) {color = "Red"}
-      else if (depth > 30) {color = "Red"}
-      else if (depth > 10) {color = "Red"}
-      else if (depth > -10) {color = "Red"}
-      else {color = "Red"}
-    
-    //  by passing the third coordinate into the markerSize function
-    // earthquakeMarkers.push(
-    var circle = L.circle(geoLat, geoLng, {
-      stroke: false,
-      fillOpacity: 0.75,
-      color: color,
-      // fillColor: "white",
-      radius: markerSize(depth),
-    }).addTo(map);
-    // );
-  };
   // create the base layers
   var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -95,6 +63,24 @@ function createMap(earthquakes) {
   // var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
   //   attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
   // });
+  // Create a separate layer group: new circle markers
+  // Loop through the cities array, and create one marker for each city object.
+  for (var i = 0; i < earthquakes.length; i++) {
+    L.circle(earthquakes[i].geometry.coordinates[0], {
+      fillOpacity: 0.75,
+      color: ,
+      fillColor: "purple",
+      // Setting our circle's radius to equal the output of our markerSize() function:
+      // This will make our marker's size proportionate to its population.
+      radius: markerSize(cities[i].population)
+    }).bindPopup(`<h1>${cities[i].name}</h1> <hr> <h3>Population: ${cities[i].population.toLocaleString()}</h3>`).addTo(myMap);
+  }
+
+
+
+
+
+  var circle = L.layerGroup(circleMarkers);
 
   // create a baseMaps object
   var baseMaps = {
@@ -122,5 +108,4 @@ function createMap(earthquakes) {
   // Add the layer control to the map.  
   }).addTo(myMap);
 
-};
-
+}
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Module 15 Challenge - a new set of tools to visualize USGS earthquake data
 
 Powered by leafletjs.com
+
+# References
+Change style of GeoJSON circle marker by feature properties https://gis.stackexchange.com/questions/220489/change-style-of-geojson-circle-marker-by-feature-properties
